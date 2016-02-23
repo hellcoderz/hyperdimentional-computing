82dd97bdccc119333e38cdc63cc8aefe62d8ea2b
vsagar200 <Vikrant_Sagar@cable.comcast.com>
2016-02-23 17:16:48 -0500

    added some code to test hyperd

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]  [net.mikera/core.matrix "0.49.0"] [net.mikera/vectorz-clj "0.43.1"]]
+  :main ^:skip-aot hyperdimentional-computing.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
--- src/hyperdimentional-computing/core.clj
+++ src/hyperdimentional-computing/core.clj
@@ -1,4 +1,38 @@
-(ns hyperdimentional-computing.core)
+(ns hyperdimentional-computing.core
+  (:require [clojure.core.matrix :as m]
+        [clojure.core.matrix.linear :as ml]))
+
+
+(m/set-current-implementation :vectorz)
+
+(def sz 100000)
+
+(defn rand-hv []
+  (let [hv (m/new-sparse-array [sz])
+        n (* 0.1 sz)]
+    (dotimes [i n]
+      (m/mset! hv (rand-int sz) 1))
+    hv))
+
+(defn cosine-sim [v1 v2]
+  (/ (m/dot v1 v2)
+     (* (ml/norm v1) (ml/norm v2))))
+
+(defn mean-add [& hvs]
+  (m/emap #(Math/round (double %))
+   (m/div (apply m/add hvs) (count hvs))))
+
+(defn xor-mul [v1 v2]
+  (->> (m/add v1 v2)
+      (m/emap #(mod % 2))))
+
+(defn hamming-dist [v1 v2]
+  (m/esum (xor-mul v1 v2)))
+
+(def a (rand-hv))
+(def b (rand-hv))
+(println (cosine-sim a b))
+
 
 (defn foo
   "I don't do a whole lot."
